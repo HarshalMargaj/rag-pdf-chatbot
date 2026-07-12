9ff4c7aff980c7abb145a0dcc60157b75f1e0356
Harshal Margaj <harshal.margaj12@gmail.com>
2026-07-12 10:32:19 +0530

    remove: sound on click

--- components/Sidebar/SidebarLogo.tsx
+++ components/Sidebar/SidebarLogo.tsx
@@ -1,4 +1,3 @@
-import { playSound } from "@/lib/PlaySound";
 import React from "react";
 import { BiSolidSquareRounded } from "react-icons/bi";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
@@ -13,7 +12,6 @@ const SidebarLogo = ({ isOpen, setIsOpen }: SidebarLogoProps) => {
 		<div className="p-4 pb-0">
 			<div
 				onClick={() => {
-					playSound();
 					setIsOpen(!isOpen);
 				}}
 				className="absolute -right-3.5 top-16 border border-[#1F1F27] rounded-full p-1 bg-[#0F0F12] cursor-pointer z-50"
